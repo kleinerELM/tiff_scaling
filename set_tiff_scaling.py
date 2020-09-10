80322c8180cb748fce575ddce0e39be71f31a9ee
Florian Kleiner <mail@floriankleiner.de>
2020-09-10 15:52:38 +0200

    improved standalone version of set_tiff_scaling.py

--- set_tiff_scaling.py
+++ set_tiff_scaling.py
@@ -40,21 +40,63 @@ if __name__ == '__main__':
     programInfo()
 
     settings = {}
-    settings["filepath"] = filedialog.askopenfilename(title='Please select the image',filetypes=[("Tiff images", "*.tif;*.tiff")])
-    settings["workingDirectory"] = os.path.dirname( settings["filepath"] )
+
+    print( "Set the scale for a single file (f) or all files in a directory (d), [f]", end=": " )
+    actionType = input()
     
-    print( "Set the unit of scaling. Input example: 2.123456" )
-    print( "Set the unit of scaling (Standard nm)", end=": " )
+    #print( 'Replace the processed file? y/n [n]', end=": " )
+    #replaceFile = input()
+    #if ( replaceFile == 'y' ): 
+    #    file_prefix = ''
+    #else:
+    #    file_prefix = 'scaled_'
+    print( 'Set the output folder name (if empty, the new file will be saved with a filename prefix)', end=": " )
+    output_folder_name = input()
+    if ( output_folder_name == '' ):
+        file_prefix = 'scaled_'
+    else:
+        output_folder_name = output_folder_name + '/'
+        file_prefix = ''
+
+
+    print( "Set the unit of scaling [nm]", end=": " )
     unit = input()
     if unit == '': unit = 'nm'
-
+    
     print( 'Set the scaling of the image. Input example: 2.123456' )
     print( 'Scaling [' + unit + '/px]', end=": " )
     scale = float( input() )
-
     scaling = { 'x' : scale, 'y' : scale, 'unit' : unit}
 
-    with Image.open( settings["workingDirectory"] + '/' + os.path.basename( settings["filepath"] ) ) as img:
-        img.save( settings["workingDirectory"] + '/Scaled_' + os.path.basename( settings["filepath"] ), tiffinfo = setImageJScaling( scaling ) ) 
+    if ( actionType == 'd' ):
+        print( "Please select a working directory", end="\r" )
+        settings["workingDirectory"] = filedialog.askdirectory(title='Please select the image / working directory')
+        
+        if ( output_folder_name != '' ):
+            if not os.path.exists(settings["workingDirectory"] + '/' + output_folder_name):
+                os.makedirs(settings["workingDirectory"] + '/' + output_folder_name)
 
+        fileCount = 0
+        position = 0
+        for file in os.listdir( settings["workingDirectory"] ):
+            if ( file.endswith(".tif") or file.endswith(".TIF")):
+                fileCount += 1
+        for file in os.listdir( settings["workingDirectory"] ):
+            if ( file.endswith(".tif") or file.endswith(".TIF")):
+                filename = os.fsdecode(file)
+                position = position + 1
+                print( " Processing " + filename + " (" + str(position) + " / " + str(fileCount) + ") :" )
+                with Image.open( settings["workingDirectory"] + '/' + filename ) as img:
+                    img.save( settings["workingDirectory"] + '/' + output_folder_name + file_prefix + filename, tiffinfo = setImageJScaling( scaling ) ) 
+        print( "The ImageJ scaling in " + str( position ) + " files is set to " + str( scale ) + " " + unit + "." )
+    else:
+        settings["filepath"] = filedialog.askopenfilename(title='Please select the image',filetypes=[("Tiff images", "*.tif;*.tiff")])
+        settings["workingDirectory"] = os.path.dirname( settings["filepath"] )
+        if ( output_folder_name != '' ):
+            if not os.path.exists(settings["workingDirectory"] + '/' + output_folder_name):
+                os.makedirs(settings["workingDirectory"] + '/' + output_folder_name)
+
+        with Image.open( settings["workingDirectory"] + '/' + os.path.basename( settings["filepath"] ) ) as img:
+            img.save( settings["workingDirectory"] + '/' + output_folder_name + file_prefix + os.path.basename( settings["filepath"] ), tiffinfo = setImageJScaling( scaling ) ) 
+        print( "The ImageJ scaling in the file './" + output_folder_name + file_prefix + os.path.basename( settings["filepath"] ) + "' is set to " + str( scale ) + " " + unit + "." )
     print( "Script DONE!" )
