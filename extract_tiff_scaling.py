4863593964125a1865d8bbfdca589fb974156b19
Florian Kleiner <mail@floriankleiner.de>
2021-02-12 16:19:04 +0100

    hide debug output

--- extract_tiff_scaling.py
+++ extract_tiff_scaling.py
@@ -12,7 +12,7 @@ def programInfo():
     print("# A Script to extract the scaling in TIFFs edited by    #")
     print("# ImageJ                                                #")
     print("#                                                       #")
-    print("# © 2020 Florian Kleiner                                #")
+    print("# © 2021 Florian Kleiner                                #")
     print("#   Bauhaus-Universität Weimar                          #")
     print("#   F. A. Finger-Institut für Baustoffkunde             #")
     print("#                                                       #")
@@ -88,18 +88,18 @@ class unit():
         factorPos = 0
         for i, factor in enumerate(self.unitFactorArrayInv):
             if ( value * factor > 1 and unit == 'px'):
-                print(value * factor)
+                #print(value * factor)
                 if i > 0: factorPos = i - 1
                 break
-        
-        print( '  {:.4f} {}/px'.format(value*self.unitFactorArrayInv[factorPos], self.unitArray[factorPos]) )
+
+        #print( '  {:.4f} {}/px'.format(value*self.unitFactorArrayInv[factorPos], self.unitArray[factorPos]) )
         return self.unitFactorArrayInv[factorPos], self.unitArray[factorPos]
 
 def getEmptyScaling():
     return { 'x' : 1, 'y' : 1, 'unit' : 'px', 'editor':None}
 
 def getImageJScaling( filename, workingDirectory, verbose = False ):
-    UC = unit() 
+    UC = unit()
     scaling = getEmptyScaling()
     with Image.open( workingDirectory + os.sep + filename ) as img:
         if ( 282 in img.tag ) and ( 283 in img.tag ):
@@ -153,21 +153,21 @@ def isFEIImage( filename, workingDirectory, verbose = False ):
 
 def getFEIScaling( filename, workingDirectory, verbose=False, save_scaled_image=False ):
     scaling = getEmptyScaling()
-    UC = unit() 
+    UC = unit()
     with tifffile.TiffFile( workingDirectory + os.sep + filename ) as tif:
-        print(tif.pages[0].tags)
+        #print(tif.pages[0].tags)
         if ( tif.fei_metadata != None ):
             if verbose: print( 'SEM image saved by an FEI / thermoScientific device' )
             scaling['editor'] = 'FEI-SEM'
             scaling['x'] = float( tif.fei_metadata['Scan']['PixelWidth'] )
             scaling['y'] = float( tif.fei_metadata['Scan']['PixelHeight'] )
-            
-            print('autodetect unit', scaling)
+
+            #print('autodetect unit', scaling)
             factor, scaling['unit'] = UC.autodetect_unit(scaling['x'])
             scaling['x'] *= factor
             scaling['y'] *= factor
-            print(scaling)
-            print(set_tiff_scaling.setImageJScaling( scaling, True ))
+            #print(scaling)
+            #print(set_tiff_scaling.setImageJScaling( scaling, True ))
 
             if save_scaled_image:
                 with Image.open( workingDirectory + os.sep + filename ) as img:
@@ -181,6 +181,7 @@ def getFEIScaling( filename, workingDirectory, verbose=False, save_scaled_image=
 def autodetectScaling( filename, workingDirectory, verbose = False ):
     scaling = getImageJScaling( filename, workingDirectory, verbose=verbose )
     if scaling['editor'] == None:
+        print('trying to detect FEI scaling')
         scaling = getFEIScaling( filename, workingDirectory, save_scaled_image=False, verbose=verbose )
     if scaling['editor'] == None:
         print( '{} was not saved using ImageJ or a SEM by FEI / thermoScientific'.format(filename) )
